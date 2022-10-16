cf33793670c29354a03ed477f0788ea62c594580
aprashant21 <aprashant21@tbc.edu.np>
2022-10-16 16:16:57 +0545

    some changes

--- home/views.py
+++ home/views.py
@@ -18,6 +18,7 @@ def about(request):
 
 resultJson = floorsheet.FloorSheetClass.floorsheet() #This is flootsheet json
 
+#This is for the JSON response
 @api_view(['GET'])
 def floorsheet(request):
     return Response(json.loads(resultJson))
